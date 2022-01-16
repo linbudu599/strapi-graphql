4e14235fa1167cc7c705dda9f430efdd5de62485
linbudu599 <linbudu599@gmail.com>
2022-01-16 14:59:51 +0800

    feat: connect client

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,24 +1,23 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 
-import { locationVar } from './router';
+import { locationVar } from "./router";
 
-const uri =
-  '/graphql';
+// const uri = "/graphql";
+
+const uri = "http://localhost:1337/graphql";
 
 export const link = new HttpLink({ uri });
 
-const cache =
-  new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          location(): Location {
-            return locationVar();
-          },
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        location(): Location {
+          return locationVar();
         },
       },
     },
-  });
+  },
+});
 
-export const client =
-  new ApolloClient({ cache, link });
+export const client = new ApolloClient({ cache, link });
--- client/src/components/app/app.ts
+++ client/src/components/app/app.ts
@@ -1,25 +1,29 @@
-import { LitElement, html, TemplateResult } from 'lit';
-import { ApolloQueryController } from '@apollo-elements/core';
-import { customElement } from 'lit/decorators.js';
+import { LitElement, html, TemplateResult } from "lit";
+import { ApolloQueryController } from "@apollo-elements/core";
+import { customElement } from "lit/decorators.js";
 
-import { AppQuery } from './App.query.graphql';
+import { AppQuery, BlogQuery } from "./App.query.graphql";
 
-import style from './app.css';
-import shared from '../shared.css';
+import style from "./app.css";
+import shared from "../shared.css";
 
-@customElement('apollo-app')
+@customElement("apollo-app")
 export class ApolloApp extends LitElement {
-  static readonly is = 'apollo-app';
+  static readonly is = "apollo-app";
 
   static readonly styles = [shared, style];
 
   query = new ApolloQueryController(this, AppQuery);
+  blogQuery = new ApolloQueryController(this, BlogQuery);
 
   render(): TemplateResult {
+    console.log(this.blogQuery);
+
     return html`
       <dl>
-        <dt>Pathname</dt>
-        <dd>${this.query.data?.location?.pathname ?? '/'}</dd>
+        <dt>
+          Blog Count: ${this.blogQuery.data?.queryBlogList.data.length ?? 0}
+        </dt>
       </dl>
     `;
   }
